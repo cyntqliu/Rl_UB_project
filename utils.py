a3c006011e4e883a6d5f3f0dcc91460718311999
Cynthia <cynthia.liu@nist.gov>
2016-08-12 16:21:32 -0400

    Update utils.py
    remember to change utils back to the original form if running anything that's not UB.

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from rllab.misc import tensor_utils
 
 
-def rollout(env, agent, max_path_length=np.inf, animated=False, speedup=1):
+def rollout(env, agent, max_path_length=10000, animated=False, speedup=1):
     observations = []
     actions = []
     rewards = []
